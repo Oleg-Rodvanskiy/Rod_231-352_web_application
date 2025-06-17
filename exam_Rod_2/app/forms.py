d5ecc8d5d9467f1c4a85074da8b41679c845dbcd
Oleg-Rodvanskiy <158483554+Oleg-Rodvanskiy@users.noreply.github.com>
2025-06-17 20:40:00 +0300

    Add files via upload

---
+++ exam_Rod_2/app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, FileField, SelectMultipleField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+class BookForm(FlaskForm):
+    title = StringField('Название', validators=[DataRequired()])
+    author = StringField('Автор', validators=[DataRequired()])
+    year = IntegerField('Год', validators=[DataRequired()])
+    publisher = StringField('Издатель', validators=[DataRequired()])
+    page_count = IntegerField('Количество страниц', validators=[DataRequired()])
+    description = TextAreaField('Описание', validators=[DataRequired()])  # Поле для описания книги
+    cover = FileField('Обложка')  # Поле для загрузки обложки
+    genres = SelectMultipleField('Жанры', coerce=int)
+    submit = SubmitField('Сохранить')
